d155f6d67b7fe31396219fa64f54e9fcba0b15e6
Avinash Kushwaha <avinash.kushwaha1501@gmail.com>
2025-09-14 18:18:43 +0530

    Seed Generation from Mnemonic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import { generateMnemonic, mnemonicToSeed } from "bip39";
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
-  
+
   return (
     <div>
       <h1>WEB_BASED_WALLET</h1>
+      <button
+        onClick={async function () {
+          const mn = generateMnemonic();
+          setMnemonic(mn);
+          const seed = await mnemonicToSeed(mn);
+          console.log(seed);
+        }}
+      >
+        Create Seed Phrase
+      </button>
+      <h2>Seed Phrase</h2>
+      <div>{mnemonic}</div>
     </div>
   );
 }
