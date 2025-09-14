b9a98b86336b36870415f7248de4812a17dc8a72
Avinash Kushwaha <avinash.kushwaha1501@gmail.com>
2025-09-14 17:31:19 +0530

    init web wallet

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [mnemonic, setMnemonic] = useState("");
+  
+  return (
+    <div>
+      <h1>WEB_BASED_WALLET</h1>
+    </div>
+  );
+}
+
+export default App;
